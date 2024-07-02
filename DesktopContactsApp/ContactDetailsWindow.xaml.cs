10cdafa40941681cf5e00abdbd3d3e13873d8747
qkrmekem <magicofclown@naver.com>
2024-07-03 00:10:53 +0900

    새로운 window추가. update, delete작업 추가

---
+++ DesktopContactsApp/ContactDetailsWindow.xaml.cs
@@ -0,0 +1,64 @@
+﻿using DesktopContactsApp.Classes;
+using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DesktopContactsApp
+{
+    /// <summary>
+    /// Interaction logic for ContactDetailsWindow.xaml
+    /// </summary>
+    public partial class ContactDetailsWindow : Window
+    {
+        Contact contact;
+        public ContactDetailsWindow(Contact contact)
+        {
+            InitializeComponent();
+
+            this.contact = contact;
+            nameTextBox.Text = contact.Name;
+            phoneTextBox.Text = contact.Phone;
+            emailTextBox.Text = contact.Email;
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+            contact.Name = nameTextBox.Text;
+            contact.Phone = phoneTextBox.Text;
+            contact.Email = emailTextBox.Text;
+
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                connection.CreateTable<Contact>();
+                connection.Update(contact);
+            }
+
+            Close();
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (SQLiteConnection connection = new SQLiteConnection(App.databasePath))
+            {
+                // 테이블을 생성, 만약 테이블이 이미 존재한다면 추가로 테이블을 생성하지 않고 무시됨
+                connection.CreateTable<Contact>();
+                // 여기서 따로 조건을 걸지 않아도contact에 존재하는 기본키를 이용해 db의 값을 제거
+                connection.Delete(contact);
+            }
+            // connection.Close();
+
+            Close();
+        }
+    }
+}
--- DesktopContactsApp/MainWindow.xaml.cs
+++ DesktopContactsApp/MainWindow.xaml.cs
@@ -18,9 +18,13 @@ namespace DesktopContactsApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Contact> contacts;
+
         public MainWindow()
         {
             InitializeComponent();
+
+            contacts = new List<Contact>();
 
             ReadDatabase();
         }
@@ -36,18 +40,48 @@ namespace DesktopContactsApp
 
         void ReadDatabase()
         {
-            List<Contact> contacts;
             using (SQLiteConnection conn = new SQLiteConnection(App.databasePath))
             {
                 conn.CreateTable<Contact>();
                 // var을 사용하는 경우 변수에 값을 바로 할당할 때에만 사용 가능
                 // var contacts = conn.Table<Contact>().ToList();
-                contacts = conn.Table<Contact>().ToList();
+                contacts = (conn.Table<Contact>().ToList()).OrderBy(c => c.Name).ToList();
+
+                var variable = from c2 in contacts
+                               orderby c2.Name
+                               select c2;
             }
 
             if(contacts != null)
             {
                 contactsListView.ItemsSource = contacts;
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TextBox searchTextBox = sender as TextBox;
+
+            var filteredList = contacts.Where(c => c.Name.ToLower().Contains(searchTextBox.Text.ToLower())).ToList();
+
+            var filteredList2 = (from c2 in contacts
+                                where c2.Name.ToLower().Contains(searchTextBox.Text.ToLower())
+                                orderby c2.Email
+                                select c2).ToList();
+
+            contactsListView.ItemsSource = filteredList;
+        }
+
+        private void contactsListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Contact selectedContact = (Contact)contactsListView.SelectedItem;
+
+            if(selectedContact != null)
+            {
+                ContactDetailsWindow contactDetailsWindow = new ContactDetailsWindow(selectedContact);
+                contactDetailsWindow.ShowDialog();
+
+                ReadDatabase();
             }
         }
     }
